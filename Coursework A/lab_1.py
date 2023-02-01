93ffd23cd8c87d845a1469711b36e53e9eebce58
mggar2124 <77509924+mggar2124@users.noreply.github.com>
2023-02-01 17:04:19 +0000

    Add files via upload

---
+++ Coursework A/lab_1.py
@@ -0,0 +1,8 @@
+password = "password"
+print("hello and welcome to lab 1 of computational inteligence")
+response = input("please input the given password \n")
+if response == password:
+    print("correct password")
+else:
+    print("password")
+    response = input("please try again")
